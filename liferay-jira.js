a5dafbec01efc475708d4ae30b657b14a59ddbc9
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2017-05-12 18:14:38 -0300

    First commit

---
+++ liferay-jira.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const JiraApi = require('jira').JiraApi;
+const FIX_PACK_REGEXP = /liferay-fixpack-portal/;
+
+class LiferayJira {
+
+  constructor(config) {
+    this.jira = new JiraApi(config.protocol || 'https',
+        config.host || 'issues.liferay.com',
+        config.port,
+        config.user,
+        config.password,
+        config.version || 'latest');
+  }
+
+  findIssue(issue) {
+    return new Promise((fulfill, reject) => {
+      this.jira.findIssue(issue, (error, issue) => {
+        if (error) return reject(error);
+        else fulfill(issue);
+      })
+    });
+  }
+
+  findLabels(issue) {
+    return this.findIssue(issue).then(issue => issue.fields.labels);
+  }
+
+  findFixPackInfo(issue) {
+    return this.findLabels(issue).then(labels => labels.filter(label => FIX_PACK_REGEXP.test(label)));
+  }
+}
+
+module.exports = LiferayJira;
